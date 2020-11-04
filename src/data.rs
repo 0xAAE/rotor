c2a62b52c7f763ad99e7917200d6831069e34ceb
avramenko.a@gmail.com <avramenko.a@gmail.com>
2020-11-04 15:49:21 +0300

    Re-implelent tree storage to use parametrized Meta and Value

--- src/data.rs
+++ src/data.rs
@@ -1,2 +1,81 @@
+use std::collections::HashMap;
+use std::fmt::Display;
+
 mod primitive;
 pub mod tree_storage;
+
+#[derive(Clone)]
+pub struct DataValue {
+    pub title: String,
+    pub name: Vec<u8>,
+    pub password: Vec<u8>,
+    pub url: String,
+    pub expire: u64,
+    pub extra: HashMap<String, Vec<u8>>,
+}
+
+impl DataValue {
+    pub fn new(title: &str, name: Vec<u8>, password: Vec<u8>) -> Self {
+        DataValue {
+            title: title.to_string(),
+            name,
+            password,
+            url: String::new(),
+            expire: 0,
+            extra: HashMap::new(),
+        }
+    }
+}
+
+impl Display for DataValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}: {}, {}-byte password",
+            self.title,
+            String::from_utf8_lossy(&self.name),
+            self.password.len()
+        )
+    }
+}
+
+#[derive(Clone)]
+pub struct NodeMeta {
+    pub title: String,
+}
+
+impl NodeMeta {
+    pub fn new(title: &str) -> Self {
+        NodeMeta {
+            title: title.to_string(),
+        }
+    }
+}
+
+impl Display for NodeMeta {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.title)
+    }
+}
+
+pub type TreeElement = tree_storage::Element<NodeMeta, DataValue>;
+
+#[test]
+fn test_data_constructing_and_display() {
+    let node_meta = NodeMeta::new("some node info");
+    let data_value = DataValue::new(
+        "some data",
+        b"data name".to_vec(),
+        b"secret password".to_vec(),
+    );
+    let mut node = TreeElement::new_node(node_meta.clone());
+    let data = TreeElement::new_data(data_value.clone());
+    assert!(node.add_child(data).is_ok());
+    // test Display
+    assert_eq!(format!("{}", node_meta), "some node info");
+    assert_eq!(
+        format!("{}", data_value),
+        "some data: data name, 15-byte password"
+    );
+    assert_eq!(format!("{}", node), "node: some node info, children: 1");
+}
--- src/data/tree_storage.rs
+++ src/data/tree_storage.rs
@@ -1,26 +1,16 @@
-use std::collections::HashMap;
 use std::fmt::Display;
-
-pub struct Value {
-    pub title: String,
-    pub name: Vec<u8>,
-    pub password: Vec<u8>,
-    pub url: String,
-    pub expire: u64,
-    pub extra: HashMap<String, Vec<u8>>,
+/// The parametrizing types can be assumed as Node **M**etadata and Data **V**alue
+pub enum Element<M, V>
+where
+    M: Display,
+    V: Display,
+{
+    Node(M, Vec<Element<M, V>>),
+    Data(V),
 }
 
-pub struct NodeMeta {
-    pub title: String,
-}
-
-pub enum Element {
-    Node(NodeMeta, Vec<Element>),
-    Data(Value),
-}
-
-impl Element {
-    pub fn add_child(&mut self, node: Element) -> Result<(), String> {
+impl<M: Display, V: Display> Element<M, V> {
+    pub fn add_child(&mut self, node: Element<M, V>) -> Result<(), String> {
         match self {
             Element::Data(_) => Err("cannot add child to data node".into()),
             Element::Node(ref _meta, ref mut children) => {
@@ -30,24 +20,12 @@ impl Element {
         }
     }
 
-    pub fn new_data(title: &str, name: Vec<u8>, password: Vec<u8>) -> Self {
-        Element::Data(Value {
-            title: title.to_string(),
-            name,
-            password,
-            url: String::new(),
-            expire: 0,
-            extra: HashMap::new(),
-        })
+    pub fn new_data(value: V) -> Self {
+        Element::Data(value)
     }
 
-    pub fn new_node(title: &str) -> Self {
-        Element::Node(
-            NodeMeta {
-                title: title.to_string(),
-            },
-            Vec::new(),
-        )
+    pub fn new_node(meta: M) -> Self {
+        Element::Node(meta, Vec::new())
     }
 
     pub fn size(&self) -> usize {
@@ -117,54 +95,109 @@ impl Element {
     }
 }
 
-impl Display for Element {
+impl<M: Display, V: Display> Display for Element<M, V> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Element::Data(ref d) => write!(f, "data {}", d.title),
+            Element::Data(ref d) => write!(f, "data {}", d),
             Element::Node(ref meta, ref children) => {
-                write!(f, "node {}, {} children", meta.title, children.len())
+                write!(f, "node: {}, children: {}", meta, children.len())
             }
         }
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::Element;
+    use std::fmt::Display;
+
+    pub struct TestValue {
+        pub title: String,
+        pub name: Vec<u8>,
+        pub password: Vec<u8>,
+    }
+
+    impl TestValue {
+        pub fn new(title: &str, name: Vec<u8>, password: Vec<u8>) -> Self {
+            TestValue {
+                title: title.to_string(),
+                name,
+                password,
+            }
+        }
+    }
+
+    impl Display for TestValue {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "TestValue")
+        }
+    }
+
+    pub struct TestMeta {
+        pub title: String,
+    }
+
+    impl TestMeta {
+        pub fn new(title: &str) -> Self {
+            TestMeta {
+                title: title.to_string(),
+            }
+        }
+    }
+
+    impl Display for TestMeta {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "TestMeta")
+        }
+    }
+
+    pub type TestElement = Element<TestMeta, TestValue>;
+}
+
 #[test]
 fn test_nodes_operations() {
+    use tests::{TestElement, TestMeta, TestValue};
     // level 2
-    let mut lvl2 = Element::new_node("lvl-2");
+    let mut lvl2 = TestElement::new_node(TestMeta::new("lvl-2"));
     assert!(lvl2
-        .add_child(Element::new_data(
+        .add_child(TestElement::new_data(TestValue::new(
             "lvl-2-0",
             b"name-2-0".to_vec(),
             b"password-2-0".to_vec()
-        ))
+        )))
         .is_ok());
-    assert!(lvl2.add_child(Element::new_node("lvl-2-1")).is_ok());
+    assert!(lvl2
+        .add_child(TestElement::new_node(TestMeta::new("lvl-2-1")))
+        .is_ok());
     // level 1
-    let mut lvl1 = Element::new_node("lvl-1");
+    let mut lvl1 = TestElement::new_node(TestMeta::new("lvl-1"));
     assert!(lvl1
-        .add_child(Element::new_data(
+        .add_child(TestElement::new_data(TestValue::new(
             "lvl-1-0",
             b"name-1-0".to_vec(),
             b"password-1-0".to_vec()
-        ))
+        )))
         .is_ok());
     assert!(lvl1.add_child(lvl2).is_ok());
     // level 0
-    let mut lvl0 = Element::new_node("lvl-0");
+    let mut lvl0 = TestElement::new_node(TestMeta::new("lvl-0"));
     assert!(lvl0
-        .add_child(Element::new_data(
+        .add_child(TestElement::new_data(TestValue::new(
             "lvl0-0",
             b"name0-0".to_vec(),
             b"password0-0".to_vec()
-        ))
+        )))
         .is_ok());
     assert!(lvl0.add_child(lvl1).is_ok());
 
     // add more nodes directly
-    assert!(lvl0.add_child(Element::new_node("lvl-0-1")).is_ok());
+    assert!(lvl0
+        .add_child(TestElement::new_node(TestMeta::new("lvl-0-1")))
+        .is_ok());
 
-    assert!(lvl0.add_child(Element::new_node("lvl-0-2")).is_ok());
+    assert!(lvl0
+        .add_child(TestElement::new_node(TestMeta::new("lvl-0-2")))
+        .is_ok());
 
     // expected structure:
     // lvl0
@@ -189,7 +222,6 @@ fn test_nodes_operations() {
             assert_eq!(value.title, "lvl0-0");
             assert_eq!(value.name, b"name0-0".to_vec());
             assert_eq!(value.password, b"password0-0".to_vec());
-            assert!(value.url.is_empty());
         }
     }
 
@@ -211,13 +243,15 @@ fn test_nodes_operations() {
     assert!(maybe_lvl_2_1.is_some());
     let lvl_2_1 = maybe_lvl_2_1.unwrap();
     assert!(lvl_2_1.is_node());
-    assert!(lvl_2_1.add_child(Element::new_node("lvl-3-0")).is_ok());
     assert!(lvl_2_1
-        .add_child(Element::new_data(
+        .add_child(TestElement::new_node(TestMeta::new("lvl-3-0")))
+        .is_ok());
+    assert!(lvl_2_1
+        .add_child(TestElement::new_data(TestValue::new(
             "lvl-3-1",
             b"name-3-1".to_vec(),
             b"password-3-1".to_vec(),
-        ))
+        )))
         .is_ok());
 
     // expected structure:
